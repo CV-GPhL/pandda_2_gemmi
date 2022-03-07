971a5c0ef01882e5fd34e541dd9235ff79ddfa0e
Clemens Vonrhein <vonrhein@globalphasing.com>
2022-03-07 09:21:09 +0000

    pick some nicer (?) palettes

--- pandda_gemmi/plots/embedding.py
+++ pandda_gemmi/plots/embedding.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import decomposition, metrics
 import umap
 from bokeh.plotting import ColumnDataSource, figure, output_file, show, save
-from bokeh.palettes import magma, viridis
+from bokeh.palettes import magma, viridis, Category20, Category10, Set1
 
 def embed_umap(reduced_array):
     distance_matrix = metrics.pairwise_distances(reduced_array)
@@ -21,12 +21,24 @@ def bokeh_scatter_plot(embedding, labels, known_apos, plot_file):
     unique_annotations = np.unique(known_apos)
     colour_map = {annotation: j for j, annotation in enumerate(unique_annotations)}
     num_unique_annotations = len(unique_annotations)
-    # pallette = magma(num_unique_annotations)
-    pallette = viridis(num_unique_annotations)
+
+    # palette = magma(num_unique_annotations)
+    if num_unique_annotations <= 2:
+        palette = Set1[num_unique_annotations]
+    elif num_unique_annotations <= 10:
+        palette = Category10[num_unique_annotations]
+    elif num_unique_annotations <= 20:
+        palette = Category20[num_unique_annotations]
+    else:
+        palette = viridis(num_unique_annotations)
+
+    if known_apos[0] == -1:
+        palette.insert(0,'#e6e6e6')
 
     for annotation in known_apos:
-        annotations.append(annotation)
-        colours.append(pallette[colour_map[annotation]])
+            annotations.append(annotation)
+            colours.append(palette[colour_map[annotation]])
+
     # for label in labels:
 
         # if label in known_apos:
